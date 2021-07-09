1719c6d6e1ecf3e8c63f7a4ff401b328b9207c1a
Garrett Breaux <garrett.breaux1@gmail.com>
2021-07-09 13:01:36 -0500

    initial commit

---
+++ ClassExercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;   
+
+namespace ClassExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            car.Make = "Ford";
+            car.Model = "Mustang";
+            car.Year = 2017;
+
+            var Cars = new List<Car>();
+            Cars.Add(car);
+
+            Console.WriteLine("Make  Model  Year");
+            Console.WriteLine("-----------------");
+
+            foreach (var item in Cars)
+            {
+                Console.WriteLine($"{item.Make} {item.Model} {item.Year}");
+            }
+
+        }
+    }
+}
